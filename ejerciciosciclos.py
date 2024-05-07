769d4b29f8c70b0df80b05bd5bdde329fe0996a5
Darkj-18 <j.juancamilojurado@gmail.com>
2024-05-07 15:59:56 -0500

    Actualiza la entrada de teclado y agrega manejo de excepciones

---
+++ ejerciciosciclos.py
@@ -0,0 +1,2 @@
+#hacer un algoritmo que me imprima los numeros del 1 al 10
+
--- uft8.py
+++ uft8.py
@@ -8,9 +8,9 @@ def utf8_to_char(utf8_code): # convierte un código UTF-8 a su caracter
 
 while True:
     try:
-        entrada = input("Ingrese una tecla o un código UTF-8 ('esc' para salir): ") # entrada por teclado
+        entrada = input("Ingrese una tecla o un código UTF-8 ('q' para salir): ")
         
-        if entrada == 'esc': # si la entrada es 'esc', se sale del programa
+        if entrada == 'q':
             print("Saliendo del programa...")
             break
         
@@ -18,9 +18,12 @@ while True:
             tecla = entrada
             codigo = char_to_utf8(tecla) # se convierte la tecla a su código UTF-8
             print(f"La tecla '{tecla}' tiene el código UTF-8: {codigo}")
-    except:
-        print("Error en la entrada. Intente nuevamente.")    
-          
-
+        else:    
+            codigo = int(entrada)  
+            tecla = utf8_to_char(codigo) # se convierte el código UTF-8 a su tecla
+            print(f"La tecla para el código UTF-8 {codigo} es: '{tecla}'") 
+    except ValueError:
+        print("Por favor ingrese una tecla valida")
 
 #keyboard.wait('esc')
+
