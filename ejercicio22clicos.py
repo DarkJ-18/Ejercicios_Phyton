88d0dd1824aeb2804a8e348f7f9a89b7160b8a9a
Darkj-18 <j.juancamilojurado@gmail.com>
2024-05-14 16:49:06 -0500

    se agregan ejercicios

---
+++ ejercicio22clicos.py
@@ -0,0 +1,11 @@
+print("Sistema de inventario")
+
+product=[]
+cantidad = []
+precio = []
+
+while True:
+    try:
+        
+    except ValueError:
+        print("Por favor ingrese un valor")
--- listas.py
+++ listas.py
@@ -47,9 +47,58 @@ print(lista)
 lista.sort()
 print(lista)
 
-# 13. Ordenar la lista en reversa
+#Ordenar la lista en reversa
 lista.sort(reverse=True)
 print(lista)
 
-# 14. Indice de un elemento
+# 13. Índice de un Elemento
 print(lista.index(4))
+
+#14  Último Elemento con pop()
+#Muestra y elimina el último elemento de la lista usando `pop()`, 
+# y luego muestra la lista modificada.
+ultimo = lista.pop()
+print(ultimo)
+print(lista)
+
+#15.  Crear Lista de Listas
+lista_de_listas = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(lista_de_listas)
+
+#16. Acceder a Elementos de Sublistas
+print(lista_de_listas[0][0])
+
+#17. Añadir Sublista
+#Añade una nueva sublista `[10, 11, 12]` usando `append()` y muestra la lista de listas.
+lista_de_listas.append([10, 11, 12])
+print(lista_de_listas)
+
+#18. Fusionar dos Listas en Una Nueva Lista
+print("---")
+lista1 = [1, 2, 3]
+lista2 = [4, 5, 6]
+lista1.extend(lista2)
+print(lista1)
+
+#19 Lista de Strings
+#Crea una lista de strings con los nombres de cinco 
+#frutas y usa sort() para ordenarlos alfabéticamente.
+frutas = ["manzana", "banana", "cereza", "dátil", "fresa"]
+frutas.sort()
+print(frutas)
+
+#20. Eliminar Elemento Específico Usando pop()
+#Elimina el segundo elemento de la lista de frutas usando pop() 
+# y muestra la lista modificada y el elemento eliminado.
+
+elemento_eliminado = frutas.pop(1)
+print(elemento_eliminado)
+print(frutas)
+
+#21. Eliminar todo el contenido de una lista clear()
+lista.clear()
+print(lista)
+
+
+
+
