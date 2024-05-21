43c75f0afbdf35e169f476cfcd594daf5746b502
Darkj-18 <j.juancamilojurado@gmail.com>
2024-05-21 14:55:11 -0500

    Agregadas funcionalidades de creación y búsqueda de productos

--- #22-crud.py
+++ #22-crud.py
@@ -15,11 +15,25 @@ while True:
         opc = int(input("Ingresa una opcion\n 1. CREAR PRODUCTO \n 2. BUSCAR PRODUCTON\n 3. ACTUALIZAR PRODUCTO\n 4. ELIMINAR PRODUCTO\n 5.SALIR "))
         
         if opc == 1:
+            print("CREANDO PRODUCTOS")
             product = input("Ingresa nombre del producto: ").capitalize()
             amount = input("Ingrese la cantidad del producto: ")
             price = float(input("ingrese el precio del producto: "))
+            
+            producto.append(product)
+            cantidad.append(amount)
+            precio.append(price)
+            
+            print("Producto registrado correctamente")
           
         elif opc == 2:
+              searchProduct = input("Ingresa el producto a buscar: ").capitalize()
+              result = searchProduct in producto
+              
+              if result == True
+              
+              
+               
               
         
     except ValueError:
