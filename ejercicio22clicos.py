3c61aae87ef4db5cd177f7d47f7bcef1dbaa1b28
DarkJ-18 <j.juancamilojurado@gmail.com>
2024-06-05 14:38:04 -0500

    Actualizar ejercicio22clicos.py

--- ejercicio22clicos.py
+++ ejercicio22clicos.py
@@ -1,16 +1,19 @@
-import os 
+import os
+
 os.system("clear")
 
-producto =[]
-cantidad =[]
-precio=[]
+producto = []
+cantidad = []
+precio = []
 
-opc=0
+opc = 0
 while opc != 9:
-    print("1-Crear un producto\n2--Buscar Un producto\n3--Actualizar Producton\n4--Eliminar producto\n5--Lista de productos\n6--Ordenar producto por nombre\n7--Invertir los productos\n8--Eliminar todos los productos\n9--Salir")
-    opc=int(input("Ingrese su opcion: "))
+    print(
+        "1-Crear un producto\n2--Buscar Un producto\n3--Actualizar Producton\n4--Eliminar producto\n5--Lista de productos\n6--Ordenar producto por nombre\n7--Invertir los productos\n8--Eliminar todos los productos\n9--Salir"
+    )
+    opc = int(input("Ingrese su opcion: "))
 
-    if opc ==1:
+    if opc == 1:
         os.system("clear")
         print("---Crear Producto---")
         nombreP = input("Ingresa el nombre del producto: ").capitalize()
@@ -19,11 +22,11 @@ while opc != 9:
 
         producto.append(nombreP)
         cantidad.append(cantidadP)
-        precio.append  (precioP)
+        precio.append(precioP)
 
         print("---Producto Agregado con exito---")
 
-    elif opc ==2:
+    elif opc == 2:
         os.system("clear")
         buscarProducto = input("Que producto deseas buscar?: ").capitalize()
         resultado = buscarProducto in producto
@@ -32,14 +35,13 @@ while opc != 9:
             print("---Producto encontrado---")
             elemento = producto.index(buscarProducto)
 
-            print("Nombre del producto ",producto[elemento])
-            print("Cantidad del producto ",cantidad[elemento])
-            print("Precio del producto ",precio[elemento])
+            print("Nombre del producto ", producto[elemento])
+            print("Cantidad del producto ", cantidad[elemento])
+            print("Precio del producto ", precio[elemento])
         else:
-                print("----- Producto No encontrado -----")
+            print("----- Producto No encontrado -----")
 
-
-    elif opc ==3:
+    elif opc == 3:
         os.system("clear")
         buscarProducto = input("Que producto deseas Actualizar?: ").capitalize()
         resultado = buscarProducto in producto
@@ -51,15 +53,15 @@ while opc != 9:
             nuevaCantidadP = int(input("Ingrese la nueva cantidad del producto: "))
             nuevoPrecioP = float(input("Ingrese el nuevo precio del producto: "))
 
-            producto[elemento]= nuevoNombreP
-            cantidad[elemento]= nuevaCantidadP
-            precio[elemento]  = nuevoPrecioP
-            
+            producto[elemento] = nuevoNombreP
+            cantidad[elemento] = nuevaCantidadP
+            precio[elemento] = nuevoPrecioP
+
             print("----- Producto actualizado correctamente -----")
         else:
-            print("----- Producto No encontrado -----") 
+            print("----- Producto No encontrado -----")
 
-    elif opc ==4:
+    elif opc == 4:
         os.system("clear")
         buscarProducto = input("Que producto deseas Eliminar?: ").capitalize()
         resultado = buscarProducto in producto
@@ -73,29 +75,29 @@ while opc != 9:
             del precio[elemento]
             print("---Elemento eliminado con exito---")
         else:
-            print("---El producto no fue encontrado---")    
+            print("---El producto no fue encontrado---")
 
-    elif opc ==5:
+    elif opc == 5:
         os.system("clear")
         if producto:
             print("---Lista de productos---")
-            for p,c,pr in zip(producto,cantidad,precio):
+            for p, c, pr in zip(producto, cantidad, precio):
                 print(f"producto: {p} Cantidad: {c} Precio: {pr}")
         else:
             print("---No hay productos para mostrar---")
-    elif opc ==6:
+    elif opc == 6:
         os.system("clear")
         if producto:
-            
-            producto,cantidad,precio = zip(*sorted(zip[producto,cantidad,precio]))
+
+            producto, cantidad, precio = zip(*sorted(zip[producto, cantidad, precio]))
 
             print("---Lista de productos en orden---")
-            for p,c,pr in zip(producto,cantidad,precio):
+            for p, c, pr in zip(producto, cantidad, precio):
                 print(f"producto: {p} Cantidad: {c} Precio: {pr}")
         else:
             print("---No hay productos para mostrar---")
 
-    elif opc ==7:
+    elif opc == 7:
         os.system("clear")
         if producto:
             producto.reverse()
@@ -103,18 +105,15 @@ while opc != 9:
             precio.reverse()
         else:
             print("---No hay productos para mostrar---")
-    elif opc ==8:
+    elif opc == 8:
         os.system("clear")
         if producto:
             producto.clear()
             cantidad.clear()
             precio.clear()
 
-            
-
-    elif opc ==9:
+    elif opc == 9:
         os.system("clear")
         print("El perico no se vende aca!!")
-        
-#------------------------------
 
+# ------------------------------
